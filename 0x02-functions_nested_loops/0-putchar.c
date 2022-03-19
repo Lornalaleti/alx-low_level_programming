8985b59922d65f5426e63b13e901882da4d93738
Lornalaleti <99435234+Lornalaleti@users.noreply.github.com>
2022-03-19 12:40:20 +0300

    Create 0-putchar.c
    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char h[] = "Main";
+	int m;
+
+	m = 0;
+
+	while (m <= 8)
+	{
+		_putchar(h[m]);
+		m++;
+	}
+	_putchar('\n');
+	return (0);
+}
