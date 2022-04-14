2e8cd5324e9e6990433d54ad3df0c83a12b893fc
Lornalaleti <99435234+Lornalaleti@users.noreply.github.com>
2022-04-14 11:09:12 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,33 +1,74 @@
-#include"3-calc.h"
+#include "3-calc.h"
 /**
- * get_op_func - get the operator and redirect to the func.
- * @s: the operator
+ * op_add - this function adds two numbers
+ * @a:the first number
+ * @b:the second number.
 (* a blank line
-* Description: get the operator and redirect to the func.)?
+* Description: this function adds two numbers)?
 (* section header: 3-calc.h)*
-* Return: return null or the function to do.
+* Return: return the result of the add.
 */
-
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
-
-	i = 0;
-	while (i < 5)
+	return (a + b);
+}
+/**
+ * op_sub - this function sub two numbers
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function sub two numbers)?
+(* section header: 3-calc.h)*
+* Return: return the result of the sub.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - this function multiply two numbers
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function multiply two numbers)?
+(* section header: 3-calc.h)*
+* Return: return the result of the multiply.
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - this function divide two numbers
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function divide two numbers)?
+(* section header: 3-calc.h)*
+* Return: return the result of the divide.
+ */
+int op_div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+	printf("Error\n");
+	exit(100);
+}
+/**
+ * op_mod - this function calcualte the module
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function calculate the module)?
+(* section header: 3-calc.h)*
+* Return: return the result of the module.
+*/
+int op_mod(int a, int b)
+{
+	if (b == 0)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-		{
-			return (*(ops[i]).f);
-		}
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-	return (NULL);
+	return (a % b);
 }
