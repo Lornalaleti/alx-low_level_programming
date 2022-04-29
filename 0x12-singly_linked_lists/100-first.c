0352e69dc1600fd894d3458a480d3d7abda8a9ff
Lornalaleti <99435234+Lornalaleti@users.noreply.github.com>
2022-04-29 11:51:22 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+* startupfun - Function to execueted before the main.
+*
+* Return: Void.
+*
+*/
+void startupfun(void)
+{
+char *str;
+str = "I bore my house upon my back!";
+printf("You're beat! and yet, you must allow,\n%s\n", str);
+}
